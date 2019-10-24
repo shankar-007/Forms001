c33b7d0c87694e520712bcfb44933b03a2e0de24
VigneshR <vigneshravichandran@hakunamatata.in>
2019-10-24 12:02:50 +0530

    project initial setup finished.

---
+++ SampleTest/SampleTest/Business Modules/Tracker/Controller/TrackerInfoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TrackerInfoViewController.swift
+//  SampleTest
+//
+//  Created by Mac HD on 24/10/19.
+//  Copyright © 2019 HMSPL. All rights reserved.
+//
+
+import UIKit
+
+class TrackerInfoViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SampleTest/SampleTest/Business Modules/Tracker/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleTest
+//
+//  Created by Mac HD on 24/10/19.
+//  Copyright © 2019 HMSPL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
